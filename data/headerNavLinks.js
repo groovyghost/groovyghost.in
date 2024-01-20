c738ec5bac87930266254ec9b99cf16ad6177aac
groovyghost <groovyghost@protonmail.com>
2024-01-20 23:25:45 +0530

    fix: cicd submodule fix

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/snippets', title: 'Snippets' },
   { href: '/about', title: 'About' },
-  { href: '/resume/resume.pdf', title: 'Resume' },
+  { href: '/resume.pdf', title: 'Resume' },
 ]
 
 export default headerNavLinks
